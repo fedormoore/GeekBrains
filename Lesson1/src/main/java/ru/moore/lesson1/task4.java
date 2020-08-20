ca4130bf27ef05e5126cf3de8b39310b5a8bd2e7
fedormoore <fedormoore@gmail.com>
2020-08-20 15:15:01 +0500

    replase task 4

--- Lesson1/src/main/java/ru/moore/lesson1/task4.java
+++ Lesson1/src/main/java/ru/moore/lesson1/task4.java
@@ -3,18 +3,14 @@ package ru.moore.lesson1;
 public class task4 {
 
     public static void main(String[] args) {
-        System.out.println(condition(5, 9));
+        System.out.println(condition(5, 5));
     }
 
     public static boolean condition(int x, int y) {
         int c = x + y;
 
-        if (c >= 10) {
-            if (c <= 20) {
-                return true;
-            } else {
-                return false;
-            }
+        if (c >= 10 && c <= 20) {
+            return true;
         } else {
             return false;
         }
