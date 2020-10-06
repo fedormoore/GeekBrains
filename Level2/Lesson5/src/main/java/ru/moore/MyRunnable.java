5f10270bd5f30c8d9123e0341bfc69d6dce42143
fedormoore <fedormoore@gmail.com>
2020-10-06 15:32:07 +0500

    Add home work from lesson 5

---
+++ Level2/Lesson5/src/main/java/ru/moore/MyRunnable.java
@@ -0,0 +1,17 @@
+package ru.moore;
+
+public class MyRunnable implements Runnable {
+
+    float[] arr;
+
+    public MyRunnable(float[] arr) {
+        this.arr = arr;
+    }
+
+    @Override
+    public void run(){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+}
