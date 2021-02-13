924e47d8ddba3a46302b2514baae45b8f36c1596
fedormoore <fedormoore@gmail.com>
2021-02-13 16:51:09 +0500

    Add home work from level 5 lesson 3

---
+++ Level5/Lesson3/NetworkClientServer/src/main/java/ru/moore/CommandType.java
@@ -0,0 +1,11 @@
+package ru.moore;
+
+public enum CommandType {
+    REG_USER,
+    REG_OK,
+    REG_ERROR,
+
+    AUTH_USER,
+    AUTH_OK,
+    AUTH_ERROR,
+}
---
+++ Level5/Lesson3/cloudClient/src/main/java/ru/moore/controller/RegDialogController.java
@@ -0,0 +1,72 @@
+package ru.moore.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import ru.moore.Command;
+import ru.moore.Main;
+import ru.moore.commands.RegErrorCommandData;
+import ru.moore.commands.RegOkCommandData;
+import ru.moore.network.Network;
+
+public class RegDialogController {
+
+    private @FXML
+    TextField userNameField;
+    private @FXML
+    TextField loginField;
+    private @FXML
+    PasswordField passwordField;
+
+    private Network network;
+    private Stage authDialogStage;
+    private Stage regDialogStage;
+
+    @FXML
+    public void executeReg(ActionEvent actionEvent) {
+        if (userNameField.getText().isEmpty() || loginField.getText().isEmpty() || passwordField.getText().isEmpty()) {
+            Main.showNetworkError("Не все поля заполнены", "Ошибка");
+            return;
+        }
+        network.onCommand(this::onCommandSuccess);
+        network.sendRegCommand(userNameField.getText(), loginField.getText(), passwordField.getText());
+    }
+
+    public void onCommandSuccess(Command networkCommand) {
+        Command command = (Command) networkCommand;
+        switch (command.getType()) {
+            case REG_OK: {
+                RegOkCommandData data = (RegOkCommandData) command.getData();
+                Platform.runLater(() -> {
+                    Main.showNetworkError(data.getStatus(), "Сообщение");
+                    regDialogStage.close();
+                    authDialogStage.show();
+                });
+                break;
+            }
+            case REG_ERROR: {
+                RegErrorCommandData data = (RegErrorCommandData) command.getData();
+                Platform.runLater(() -> {
+                    Main.showNetworkError(data.getErrorMessage(), "Ошибка");
+                });
+                break;
+            }
+        }
+    }
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+
+    public void setAuthDialogStage(Stage authDialogStage) {
+        this.authDialogStage = authDialogStage;
+    }
+
+    public void setRegDialogStage(Stage regDialogStage) {
+        this.regDialogStage = regDialogStage;
+    }
+
+}
