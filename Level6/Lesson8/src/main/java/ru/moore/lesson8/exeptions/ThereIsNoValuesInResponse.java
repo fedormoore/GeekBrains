b7a2402895927db30fb50dccec806692fc0193f4
fedormoore <fedormoore@gmail.com>
2021-03-14 14:10:52 +0500

    add home work

--- Level6/Lesson8/src/main/java/ru/moore/lesson8/exeptions/ThereIsNoValuesInResponse.java
+++ Level6/Lesson8/src/main/java/ru/moore/lesson8/exeptions/ThereIsNoValuesInResponse.java
@@ -1,4 +1,4 @@
-package ru.moore.lesson7.exeptions;
+package ru.moore.lesson8.exeptions;
 
 public class ThereIsNoValuesInResponse extends RuntimeException {
     public ThereIsNoValuesInResponse(String message) {
