3c720e20fda2204527320431df5bbaed2272d0fc
fedormoore <fedormoore@gmail.com>
2021-05-04 08:48:14 +0500

    1

---
+++ Level7/Lesson4/core/src/main/java/ru/moore/market/core/models/dtos/AuthResponseDto.java
@@ -0,0 +1,12 @@
+package ru.moore.market.core.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponseDto {
+
+    private String token;
+
+}
