35642287a884c515c22e8eecde3ed35ee88c1e4c
fedormoore <fedormoore@gmail.com>
2021-08-24 08:34:32 +0500

    Add home work from level 9 lesson 6

---
+++ Level9/Lesson6/src/main/java/ru/moore/lesson6/HelloServlet.java
@@ -0,0 +1,22 @@
+package ru.moore.lesson6;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello World");
+    }
+}
